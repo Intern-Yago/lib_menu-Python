5bff1e6fe6f809fca1ef18270c1928d6fe2f99e3
Syri-Yagowo <yagovictorbotafogo@gmail.com>
2021-04-29 22:13:11 -0300

    demosntrando os pacotes disponiveis

---
+++ __init__.py
@@ -0,0 +1,37 @@
+"""
+------------------------------------------------------------------------------------------------------------------
+                                                menuArquivos_py
+------------------------------------------------------------------------------------------------------------------
+                                                 Syri_Yagowo©
+
+Este pacote irá te ajudar na construção de um menu com opções que irão ser salvos em um arquivo .txt para poder
+facilitar os acessos futuros, tanto por dentro do programa, quanto por fora para manutenções.
+Possue funções que irão fazer apenas os ajustes visuais como formatação, provocar diminuição da possibilidade de erros,
+tanto por parte do usuário, quanto por parte do desenvolvedor e melhorar a funcionalidade de módulos
+
+Criado para iniciantes em python e segue a mesma ideologia de simplicidade e códigos intuitivos
+Caso mesmo assim ainda precise de ajuda, utilize o comando help() para obter informações sobre sua dúvida
+
+*************************************   Ainda em ajustes   **************************************
+Planos futuros:
+°Adição de senhas com criptografia para usuários
+°Limitação de informações para usuários tradicionais com senha e para os que não possuem a senha
+    ->Retirada do usuário caso possua a senha do mesmo
+    ->Mudança de senha e nome de usuário
+°Criação do módulo para acesso de administrador
+    -> Este terá acesso a senhas, usuários e todas suas informações colocadas no programa
+    -> Retirada de usuário
+**************************************************************************************************
+
+Se tiver dicas adicione <seu nome/nickname>:<sua contribuição>
+EX.: Syri_Yagowo : Adicione uma função que servirá como o help, mas de forma mais bela
+
+Peço para que não abusem da liberdade e confundam com libertinagem
+Este pacote foi desenvolvido para facilitar a complexidade que programa desse gênero pode ter
+
+Muito obrigado e espero que faça bom proveito
+
+ASS:Syri_Yagowo
+"É A VIDA! E ela não para"
+"""
+__all__ = ['cadastrar', 'leiaIntFloat', 'manipulandoTXT', 'menu','cadastrar_Mysql']
